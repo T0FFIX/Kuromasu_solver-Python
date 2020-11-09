2fd6b2a3f4b42084c119578178b287bf04bf59b6
T0FFIX <adriantrapp9@gmail.com>
2020-11-09 03:53:56 +0100

    changes in the main.py function

--- main.py
+++ main.py
@@ -5,84 +5,129 @@ import sys
 mapsFolder = "maps/"  # folder with maps to read from
 mapsAnswersFolder = "maps_answers/"  # folder with answers
 
+
 def extractMap(mapName):
     file = open(mapName, "r")
     data = file.read()  # read whole file to string
     file.close()
     temp = data.split(',')  # split data to temoporary 1d array
 
-    arr = []
-    counter = 2  # actual position in splitted array, width and height is not counted
     width = int(temp[0])
     height = int(temp[1])
-    for i in range(height):
-        col = []
-        for j in range(width):
-            col.append(arr[counter])
-            counter += 1
-        arr.append(col)
+
+    arr = []
+    temp = temp[2:]  # rid off unnessesary width and height
+    for i in range(0, width * height - 2):
+        arr.append(str(temp[i]))
 
     return arr
 
-def checkScore(playerBoard, correctMap):
+
+def checkQuality(actualBoard, answerBoard):
     qualityScore = 0
-    for col in range(0, len(correctMap)):
-        for row in range(0, len(correctMap[col])):
-            if playerBoard[col][row] != correctMap[col][row]:
-                qualityScore += 1
+    for i in range(0, len(actualBoard)):
+        if actualBoard[i] != answerBoard[i]:
+            qualityScore += 1
 
-    return qualityScore  # returns number of mistakes, 0 is ideal solution
+    return qualityScore
 
-def randomGeneretedSolution(playerBoard):
-    for col in range(0, len(playerBoard)):
-        for row in range(0, len(playerBoard[col])):
-            if playerBoard[col][row] == 0:
-                playerBoard[col][row] = random.randint(0, 1)
+
+def generateRandomSolution(playerBoard): #playerBoard => cleanBoard
+    for i in range(0, len(playerBoard)):
+        if playerBoard[i] == str(0):
+            playerBoard[i] = str(random.randint(0, 1))
 
     return playerBoard
 
-def forcedSolution(solution):
-    return 0
 
-def printSolution(cleanBoard, actualBoard, mistakesNumber):
-    for col in range(0, len(cleanBoard)):
-        for row in range(0, len(cleanBoard[col])):
-            print(cleanBoard[col][row])
+def printSolution(cleanBoard, actualBoard, height, width):
+    print("Input: ")
+    for el in cleanBoard:
+        print(el + " ", end="")
 
-    for col in range(0, len(actualBoard)):
-        for row in range(0, len(actualBoard[col])):
-            print(actualBoard[col][row])
+    print("\n" + "Output: ")
+    for el in actualBoard:
+        print(el + " ", end="")
 
-    print(mistakesNumber)
+
+def getMapSizes(mapName):
+    file = open(mapName, "r")
+    data = file.read()  # read whole file to string
+    file.close()
+    temp = data.split(',')  # split data to temoporary 1d array
+
+    width = int(temp[0])
+    height = int(temp[1])
+
+    arr = [width, height]
+    return arr
+
 
 def saveSolution(solution, filename):
     file = open(filename, "w")
-    for col in range(0, len(solution)):
-        for row in range(0, len(solution[col])):
-            file.write(solution[col][row])
+    for i in range(0, len(solution)):
+        file.write(solution[i])
     file.close()
 
+
+def appendWithZeros(element, size):
+    new = ""
+    for i in range(0, size - len(element)):
+        new += str(0)
+
+    new += element
+    return list(new)
+
+
+# size is number of white squares on map
+def getBinaryPermutations(size):
+    collection = []
+    for i in range(0, 2 ** size):
+        record = str(bin(i))
+        collection.append(appendWithZeros(record[2:], size))
+
+    return collection
+
+
+def insertNumbers(cleanBoard, answerBoard):
+    for i in range(len(cleanBoard)):
+        if int(cleanBoard[i]) >= 2:
+            answerBoard[i] = cleanBoard[i]
+
+
+def bruteForce(cleanBoard, actualAnswerBoard):
+    size = len(cleanBoard)
+    answerBoard = getBinaryPermutations(size)
+
+    for actualBoard in answerBoard:
+        insertNumbers(cleanBoard, actualBoard)
+        if checkQuality(actualBoard, actualAnswerBoard) == 0:
+            return actualBoard
+
+
 def main():
-    mapName = "/maps/1.txt"
+    filename = "1.txt"
     mapOutput = "output.txt"
+    mapAnswersName = mapsAnswersFolder + filename
+
     if len(sys.argv) >= 2:
-        mapName = sys.argv[1]
+        filename = sys.argv[1]
         mapOutput = sys.argv[2]
 
+    mapName = mapsFolder + filename
+
     cleanBoard = extractMap(mapName)  # empty map
-    correctBoard = extractMap(mapName)  # fully correct map
+    actualAnswerBoard = extractMap(mapAnswersName)
 
     start_time = time.time()  # when alghoritm stats working
-    actualBoard = randomGeneretedSolution(cleanBoard)
-    mistakesNumber = checkScore(actualBoard, correctBoard)
-    while mistakesNumber != 0:
-        actualBoard = randomGeneretedSolution(cleanBoard)
-        printSolution(cleanBoard, actualBoard, mistakesNumber)
-        mistakesNumber = checkScore(actualBoard, correctBoard)
-
+    correctBoard = bruteForce(cleanBoard, actualAnswerBoard)
     work_time = time.time() - start_time
+
     print("Elapsed alghoritm work time: " + str(work_time))
-    saveSolution(actualBoard, mapOutput)
+    mapSizes = getMapSizes(mapName)
+    printSolution(cleanBoard, correctBoard, mapSizes[0], mapSizes[1])
+    saveSolution(correctBoard, mapOutput)
+
 
 if __name__ == "__main__":
     main()
