13d0558ce2d750ea3d8029d58025df3c48b539ee
T0FFIX <adriantrapp9@gmail.com>
2020-11-14 22:30:30 +0100

    check solution works and all rules for the game are working also bruteforce and random solution are more efficient and output is created in a separate file with working time

--- main.py
+++ main.py
@@ -14,93 +14,120 @@ def checkQuality(board, width, height):
         if board[i] != 1 and board[i] != 0:
             count = 0  # it automaticaly countes itself
             iterator = 0
-            # i = 0
-            #0,0,0,0,3,0,2,0,0,3,0,4,0,0,0,0 ///4
-
             # check right
+            # WORKING
             while True:
-                if i % width + iterator < width and board[i + iterator] != 1:
-                    count += 1
+                try:
                     iterator += 1
-                else:
+                    if i % width + iterator < width and board[i + iterator] != 1:
+                        count += 1
+                    elif i % width + iterator >= width or board[i + iterator] == 1:
+                        break
+                except IndexError:
                     break
             iterator = 0
+
             # check down
+            #WORKING
             while True:
-                if i + width*iterator < width * height and board[i + width*iterator] != 1:
-                    count += 1
+                try:
                     iterator += 1
-                else:
+                    if i + width * iterator <= width*height and board[i + width * iterator] != 1:
+                        count += 1
+                    elif i + width * iterator > width*height or board[i + width * iterator] == 1:
+                        break
+                except IndexError:
                     break
             iterator = 0
+
             # check left
+            # WORKING
             while True:
-                # if i-iterator >= 0:  # todo TO ZMIENIONE INNE OD RESZTY
-                if i % width - iterator < width and i - iterator >= 0 and board[i - iterator] != 1:
-                    count += 1
+                try:
                     iterator += 1
-                else:
+                    if i % width - iterator >= 0 and board[i - iterator] != 1:
+                        count += 1
+                    elif i % width - iterator < 0 or board[i - iterator] == 1:
+                        break
+                except IndexError:
                     break
             iterator = 0
+
             # check up
+            #WORKING
             while True:
-                if i - width*iterator < width * height and i - width*iterator >= 0 and board[i - width*iterator] != 1:
-                    count += 1
+                try:
                     iterator += 1
-                else:
+                    if i - width * iterator >= 0 and board[i - width * iterator] != 1:
+                        count += 1
+                    elif i - width * iterator < 0 or board[i - width * iterator] == 1:
+                        break
+                except IndexError:
                     break
-            count = count - 3
+
+            count = count + 1
             if board[i] != count:
                 error += 1
-                break
+                # break
 
         # Rule 2: No two black cells may be horizontally or vertically adjacent.
         elif board[i] == 1:  # check self if on black cell
-            if board[i+1] is not None and board[i+1] == 1:  # check right
-                error += 1
-                break
-            elif board[i+1] is not None and board[i+width] == 1:  # check down
-                error += 1
-                break
-            elif board[i+1] is not None and board[i-1] == 1:  # check left
-                error += 1
-                break
-            elif board[i+1] is not None and board[i-width] == 1:  # check up
-                error += 1
-                break
+            try:
+                if (i % width) + 1 < width and board[i + 1] == 1:  # check right
+                    error += 1
+
+                elif i + width < width * height and board[i + width] == 1:  # check down
+                    error += 1
+
+                elif (i % width) - 1 > 0 and board[i - 1] == 1: # check left
+                    error += 1
+
+                elif i - width > 0 and board[i - width] == 1:  # check up
+                    error += 1
+
+            except IndexError:
+                print("", end="")
 
         # Rule 3: All the white cells must be connected horizontally or vertically.
         elif board[i] == 0:
             counter = 0
 
-            # check right
-            if board[i + 1] is None:
-                counter += 1
-            elif board[i + 1] == 1:
-                counter += 1
-
-            # check down
-            if board[i + width] is None:
-                counter += 1
-            elif board[i + width] == 1:
+            try:
+                if board[i + 1] == 1:  # check right
+                    counter += 1
+                elif (i % width) + 1 > width:
+                    counter += 1
+            except IndexError:
                 counter += 1
 
-            # check left
-            if board[i - 1] is None:
-                counter += 1
-            elif board[i - 1] == 1:
+            try:
+                if board[i + width] == 1:  # check down
+                    counter += 1
+                elif i + width > width * height:
+                    counter += 1
+            except IndexError:
                 counter += 1
 
-            # check up
-            if board[i - width] is None:
+            try:
+                if board[i - 1] == 1: # check left
+                    counter += 1
+                elif (i % width) - 1 < 0:
+                    counter += 1
+            except IndexError:
                 counter += 1
-            elif board[i - width] == 1:
+
+            try:
+                if board[i - width] == 1:  # check up
+                    counter += 1
+                elif i - width < 0:
+                    counter += 1
+            except IndexError:
                 counter += 1
 
             if counter == 4:
                 error += 1
-
     # Return number of errors
+    # print(error)
     return error
 
 
@@ -110,36 +137,15 @@ def extractMap(mapName):
     file.close()
     temp = data.split(',')  # split data to temoporary 1d array
 
-    width = int(temp[0])
     height = int(temp[1])
+    width = int(temp[0])
 
     arr = []
     temp = temp[2:]  # rid off unnessesary width and height
-    for i in range(0, width * height - 2):
-        arr.append(str(temp[i]))
+    for i in range(0, width * height):
+        arr.append(int(temp[i]))
 
     return arr
-
-
-def generateRandomSolution(board, width, height):  # playerBoard => cleanBoard
-    errors = checkQuality(board, width, height)
-    while errors != 0:
-        for i in range(0, len(board)):
-            if board[i] == str(0):  # TODO przekonwertowac wszystkie stringi do numerow w projekcie
-                board[i] = str(random.randint(0, 1))
-        print(errors)  # TODO debug
-
-    return board
-
-
-def printSolution(cleanBoard, actualBoard, height, width):  # todo fancy feature, poprawic na 2d
-    print("Input: ")
-    for el in cleanBoard:
-        print(el + " ", end="")
-
-    print("\n" + "Output: ")
-    for el in actualBoard:
-        print(el + " ", end="")
 
 
 def getMapSizes(mapName):
@@ -148,17 +154,54 @@ def getMapSizes(mapName):
     file.close()
     temp = data.split(',')  # split data to temoporary 1d array
 
-    width = int(temp[0])
-    height = int(temp[1])
+    width = int(temp[1])
+    height = int(temp[0])
 
     arr = [width, height]
     return arr
 
 
-def saveSolution(solution, filename):
-    file = open(filename, "w")
-    for i in range(0, len(solution)):
-        file.write(solution[i])
+def generateRandomSolution(board, width, height):  # playerBoard => cleanBoard
+    usedBoard = board.copy()
+    errors = checkQuality(usedBoard, width, height)
+    while errors != 0:
+        for i in range(0, len(usedBoard)):
+            if usedBoard[i] == 0 or usedBoard[i] == 1:
+                usedBoard[i] = random.randint(0, 1)
+        errors = checkQuality(usedBoard, width, height)
+    return usedBoard
+
+
+def printSolution(cleanBoard, correctBoard, width, height):
+    print("Input: ")
+    for k in range(0, height):
+        for i in range(0, width):
+            print(str(cleanBoard[k * width + i]) + " ", end="")
+        print("")
+
+    print("\n" + "Output: ")
+    for k in range(0, height):
+        for i in range(0, width):
+            print(str(correctBoard[k * width + i]) + " ", end="")
+        print("")
+
+def saveSolution(cleanBoard, correctBoard, width, height, work_time, mapOutput):
+    file = open(mapOutput, "w")
+
+    file.write("Input:" + "\n")
+    for k in range(0, height):
+        for i in range(0, width):
+            file.write(str(cleanBoard[k * width + i]) + " ")
+        file.write("\n")
+
+    file.write("\n" +"Solution:" + "\n")
+    for k in range(0, height):
+        for i in range(0, width):
+            file.write(str(correctBoard[k * width + i]) + " ")
+        file.write("\n")
+
+    file.write("\n" + "Time:" + "\n")
+    file.write(str(work_time))
     file.close()
 
 
@@ -178,14 +221,14 @@ def insertNumbers(board, cleanBoard):
     return board
 
 
-def bruteForce(cleanBoard, width, height):
+def bruteForce(cleanBoard, width, height):  # TODO zepsute, naprawic
     for i in range(0, 2 ** len(cleanBoard)):
         board = []
         generated = str(bin(i))
         generated = appendWithZeros(generated[2:], len(cleanBoard))
 
         for letter in generated:
-            board.append(int(letter))  # todo jak nie zadziala daj extend zamiast append
+            board.append(int(letter))
         board = insertNumbers(board, cleanBoard)
 
         if checkQuality(board, width, height) == 0:
@@ -206,13 +249,16 @@ def main():
     height = getMapSizes(mapName)[1]
 
     start_time = time.time()  # when alghoritm stats working
-    # correctBoard = generateRandomSolution(cleanBoard, width, height)
-    correctBoard = bruteForce(cleanBoard, width, height)
+    correctBoard = generateRandomSolution(cleanBoard, width, height)
+    # correctBoard = bruteForce(cleanBoard, width, height)
+
     work_time = time.time() - start_time
 
-    print("Elapsed alghoritm work time: " + str(work_time))
     printSolution(cleanBoard, correctBoard, width, height)
-    saveSolution(correctBoard, mapOutput)
+    print()
+    print("Elapsed alghoritm work time: " + str(work_time))
+
+    saveSolution(cleanBoard, correctBoard, width, height, work_time, mapOutput)
 
 
 if __name__ == "__main__":
